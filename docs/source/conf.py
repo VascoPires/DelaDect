685beb88f483d7149dd45c708825d43662865440
Vasco Pires <80071904+VascoPires@users.noreply.github.com>
2025-09-19 11:20:32 +0200

    test_deladect_logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,3 +25,8 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = 'docs/images/deladect_logo.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
